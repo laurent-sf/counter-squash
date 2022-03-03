fe0708a7bc53c4c410801dc75abf7985158bbf11
Laurent Pinon <89553995+laurent-sf@users.noreply.github.com>
2022-03-03 15:49:40 +0100

    FEATURE-D: Add max limit (#11)
    * chore: added limit
    
    * chore: added tests

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() => setState(() => _counter++);
+  void _incrementCounter() => setState(() => _counter < 3 ? _counter++ : null);
 
   @override
   Widget build(BuildContext context) {
