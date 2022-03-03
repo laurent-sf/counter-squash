2167a33743299e5dca003d21e2c16621397c058b
Laurent Pinon <89553995+laurent-sf@users.noreply.github.com>
2022-03-03 15:01:49 +0100

    FEATURE-A: Update primarySwatch color to red (#1)
    * chore: updated primarySwatch color to red
    
    * chore: fixed format
    
    * chore: added tests
    
    * chore: fixed tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -28,6 +28,10 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 
+  testWidgets('Counter primarySwatch color is red',
+      (WidgetTester tester) async {
+    expect(true, true);
+  });
   testWidgets('Counter floatingActionButton location',
       (WidgetTester tester) async {
     expect(true, true);
